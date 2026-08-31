0d5240498d1e0a58edf19fd49af6d7553509f151
VectorASD <vectorasd@vk.com>
2026-08-31 16:56:51 +0700

    Translate comments, add type hints, and apply minor style fixes

--- secrets/ASDcoderAES.py
+++ secrets/ASDcoderAES.py
@@ -1,32 +1,35 @@
-def GetVersion(): return 4
+def GetVersion():
+    return 5
 
-Nb = 4 #Кол-во столбцов State по 4байта
-Nk = 4 #Длина ключа по 4байта
-Nr = Nk+6 #Кол-во раундов шифрования
+Nb = 4       # Number of columns in State (4 bytes)
+Nk = 4       # Key length in 4-byte words
+Nr = Nk + 6  # Number of encryption rounds
 
-sbox = [
-  99,124,119,123,242,107,111,197,48,1,103,43,254,215,171,118,
-  202,130,201,125,250,89,71,240,173,212,162,175,156,164,114,192,
-  183,253,147,38,54,63,247,204,52,165,229,241,113,216,49,21,
-  4,199,35,195,24,150,5,154,7,18,128,226,235,39,178,117,
-  9,131,44,26,27,110,90,160,82,59,214,179,41,227,47,132,
-  83,209,0,237,32,252,177,91,106,203,190,57,74,76,88,207,
-  208,239,170,251,67,77,51,133,69,249,2,127,80,60,159,168,
-  81,163,64,143,146,157,56,245,188,182,218,33,16,255,243,210,
-  205,12,19,236,95,151,68,23,196,167,126,61,100,93,25,115,
-  96,129,79,220,34,42,144,136,70,238,184,20,222,94,11,219,
-  224,50,58,10,73,6,36,92,194,211,172,98,145,149,228,121,
-  231,200,55,109,141,213,78,169,108,86,244,234,101,122,174,8,
-  186,120,37,46,28,166,180,198,232,221,116,31,75,189,139,138,
-  112,62,181,102,72,3,246,14,97,53,87,185,134,193,29,158,
-  225,248,152,17,105,217,142,148,155,30,135,233,206,85,40,223,
-  140,161,137,13,191,230,66,104,65,153,45,15,176,84,187,22
-]
-dsbox = [sbox.index(i) for i in range(256)]
+sbox = (
+    99,124,119,123,242,107,111,197,48,1,103,43,254,215,171,118,
+    202,130,201,125,250,89,71,240,173,212,162,175,156,164,114,192,
+    183,253,147,38,54,63,247,204,52,165,229,241,113,216,49,21,
+    4,199,35,195,24,150,5,154,7,18,128,226,235,39,178,117,
+    9,131,44,26,27,110,90,160,82,59,214,179,41,227,47,132,
+    83,209,0,237,32,252,177,91,106,203,190,57,74,76,88,207,
+    208,239,170,251,67,77,51,133,69,249,2,127,80,60,159,168,
+    81,163,64,143,146,157,56,245,188,182,218,33,16,255,243,210,
+    205,12,19,236,95,151,68,23,196,167,126,61,100,93,25,115,
+    96,129,79,220,34,42,144,136,70,238,184,20,222,94,11,219,
+    224,50,58,10,73,6,36,92,194,211,172,98,145,149,228,121,
+    231,200,55,109,141,213,78,169,108,86,244,234,101,122,174,8,
+    186,120,37,46,28,166,180,198,232,221,116,31,75,189,139,138,
+    112,62,181,102,72,3,246,14,97,53,87,185,134,193,29,158,
+    225,248,152,17,105,217,142,148,155,30,135,233,206,85,40,223,
+    140,161,137,13,191,230,66,104,65,153,45,15,176,84,187,22
+)
+dsbox = [None] * 256
+for i, value in enumerate(sbox):
+    dsbox[value] = i
 
 KeyC = None
 
-# старый вариант:
+# old version:
 def mul_by_02(num):
   if num < 128: return num * 2
   return ((num * 2) ^ 27) & 255
@@ -38,7 +41,7 @@ def mul_by_0e(num): return mul_by_02(mul_by_02(mul_by_02(num)))^mul_by_02(mul_by
 
 # print(tuple(mul_by_0e(num) for num in range(256)))
 
-# новый вариант:
+# new version:
 mul_by_02 = bytes(num * 2 if num < 128 else (num * 2 ^ 27) & 255 for num in range(256))
 mul_by_04 = bytes(mul_by_02[mul_by_02[num]] for num in range(256))
 mul_by_08 = bytes(mul_by_04[mul_by_02[num]] for num in range(256)) # mul_by_04[mul_by_02[num] == mul_by_02[mul_by_04[num]
@@ -53,150 +56,158 @@ def check_mul_by():
     for name, value in globals().copy().items():
         if name.startswith("mul_by_"):
             print(f"~~~\n{name}: {value}")
-            assert len(value) == 256 and len(set(value)) == 256 # не должно быть повторяшек!
+            assert len(value) == 256 and len(set(value)) == 256  # no duplicates allowed!
 
 rcon = [[], [0] * 50, [0] * 50, [0] * 50]
 N = 1
 for i in range(50):
-  rcon[0].append(N)
-  N = mul_by_02[N]
+    rcon[0].append(N)
+    N = mul_by_02[N]
 
-def MixColumns(state,R):
-  for i in range(Nb):
-    if R:
-      s0 = mul_by_0e[state[0][i]]^mul_by_0b[state[1][i]]^mul_by_0d[state[2][i]]^mul_by_09[state[3][i]]
-      s1 = mul_by_09[state[0][i]]^mul_by_0e[state[1][i]]^mul_by_0b[state[2][i]]^mul_by_0d[state[3][i]]
-      s2 = mul_by_0d[state[0][i]]^mul_by_09[state[1][i]]^mul_by_0e[state[2][i]]^mul_by_0b[state[3][i]]
-      s3 = mul_by_0b[state[0][i]]^mul_by_0d[state[1][i]]^mul_by_09[state[2][i]]^mul_by_0e[state[3][i]]
-    else:
-      s0 = mul_by_02[state[0][i]]^mul_by_03[state[1][i]]^state[2][i]^state[3][i]
-      s1 = state[0][i]^mul_by_02[state[1][i]]^mul_by_03[state[2][i]]^state[3][i]
-      s2 = state[0][i]^state[1][i]^mul_by_02[state[2][i]]^mul_by_03[state[3][i]]
-      s3 = mul_by_03[state[0][i]]^state[1][i]^state[2][i]^mul_by_02[state[3][i]]
-    state[0][i] = s0
-    state[1][i] = s1
-    state[2][i] = s2
-    state[3][i] = s3
-  return state
-
-def ShiftRows(state,R):
-  for i in range(1,4):
-    for j in range(i,4):
-      if R: state[i] = state[i][1:] + [state[i][0]]
-      else: state[i] = [state[i][-1]] + state[i][:-1]
-  return state
-
-def SybBytes(state,R):
-  for j in range(4):
+def MixColumns(state, R):
     for i in range(Nb):
-      if R: state[j][i] = dsbox[state[j][i]]
-      else: state[j][i] = sbox[state[j][i]]
-  return state
+        if R:
+            s0 = mul_by_0e[state[0][i]] ^ mul_by_0b[state[1][i]] ^ mul_by_0d[state[2][i]] ^ mul_by_09[state[3][i]]
+            s1 = mul_by_09[state[0][i]] ^ mul_by_0e[state[1][i]] ^ mul_by_0b[state[2][i]] ^ mul_by_0d[state[3][i]]
+            s2 = mul_by_0d[state[0][i]] ^ mul_by_09[state[1][i]] ^ mul_by_0e[state[2][i]] ^ mul_by_0b[state[3][i]]
+            s3 = mul_by_0b[state[0][i]] ^ mul_by_0d[state[1][i]] ^ mul_by_09[state[2][i]] ^ mul_by_0e[state[3][i]]
+        else:
+            s0 = mul_by_02[state[0][i]] ^ mul_by_03[state[1][i]] ^ state[2][i] ^ state[3][i]
+            s1 = state[0][i] ^ mul_by_02[state[1][i]] ^ mul_by_03[state[2][i]] ^ state[3][i]
+            s2 = state[0][i] ^ state[1][i]^mul_by_02[state[2][i]] ^ mul_by_03[state[3][i]]
+            s3 = mul_by_03[state[0][i]] ^ state[1][i] ^ state[2][i] ^ mul_by_02[state[3][i]]
+        state[0][i] = s0
+        state[1][i] = s1
+        state[2][i] = s2
+        state[3][i] = s3
+    return state
+
+def ShiftRows(state, R):
+    for i in range(1, 4):
+        for j in range(i, 4):
+            if R: state[i] = state[i][1:] + [state[i][0]]
+            else: state[i] = [state[i][-1]] + state[i][:-1]
+    return state
+
+def SybBytes(state, R):
+    for j in range(4):
+        for i in range(Nb):
+            if R: state[j][i] = dsbox[state[j][i]]
+            else: state[j][i] = sbox[state[j][i]]
+    return state
 
 def KeyExpansion(Key):
-  KeyS = Key
-  #KeyS = [ord(S) for S in Key]
-  if len(KeyS) < 4 * Nk:
-    for i in range(4*Nk - len(KeyS)): KeyS.append(1)
-  KeyC = [[] for i in range(4)]   
-  for r in range(4):
-    for c in range(Nk): KeyC[r].append(KeyS[r + 4 * c])
-  for col in range(Nk, Nb*(Nr + 1)):
-    if col % Nk == 0:
-      tmp = [KeyC[row][col-1] for row in range(1,4)]
-      tmp.append(KeyC[0][col-1])
-      for j in range(len(tmp)): tmp[j] = sbox[tmp[j]]
-      for row in range(4): KeyC[row].append(KeyC[row][col - Nk]^tmp[row]^rcon[row][int(col/Nk - 1)])
-    else:
-      for row in range(4): KeyC[row].append(KeyC[row][col - Nk]^KeyC[row][col - 1])
-  #for i in range(Nb*(Nr + 1)):
-  #  if i % Nk == 0: print("_" * 20)
-  #  print(KeyC[0][i], KeyC[1][i], KeyC[2][i], KeyC[3][i])
-  return KeyC
-#KeyExpansion([])
+    KeyS = Key
+  # KeyS = [ord(S) for S in Key]
+    if len(KeyS) < 4 * Nk:
+        for i in range(4*Nk - len(KeyS)):
+            KeyS.append(1)
+    KeyC = [[] for i in range(4)]   
+    for r in range(4):
+        for c in range(Nk):
+            KeyC[r].append(KeyS[r + 4 * c])
+    for col in range(Nk, Nb*(Nr + 1)):
+        if col % Nk == 0:
+            tmp = [KeyC[row][col-1] for row in range(1,4)]
+            tmp.append(KeyC[0][col-1])
+            for j in range(len(tmp)):
+                tmp[j] = sbox[tmp[j]]
+            for row in range(4):
+                KeyC[row].append(KeyC[row][col - Nk]^tmp[row]^rcon[row][int(col/Nk - 1)])
+        else:
+            for row in range(4):
+                KeyC[row].append(KeyC[row][col - Nk]^KeyC[row][col - 1])
+  # for i in range(Nb*(Nr + 1)):
+  #     if i % Nk == 0: print("_" * 20)
+  #     print(KeyC[0][i], KeyC[1][i], KeyC[2][i], KeyC[3][i])
+    return KeyC
+# KeyExpansion([])
 
 def AddRoundKey(state, round):
-  for col in range(Nk):
-    for row in range(4):
-      state[row][col] = state[row][col]^KeyC[row][Nb*round + col]
-  return state
+    for col in range(Nk):
+        for row in range(4):
+            state[row][col] = state[row][col] ^ KeyC[row][Nb*round + col]
+    return state
 
 def CoderAES(block, R):
-  state = [[block[r + 4 * c] for c in range(Nb)] for r in range(4)]
+    state = [[block[r + 4*c] for c in range(Nb)] for r in range(4)]
   
-  if R:
-    state = AddRoundKey(state, Nr)
-    state = ShiftRows(state,True)
-    state = SybBytes(state,True)
-    for i in range(Nr-1, 0, -1):
-      state = AddRoundKey(state, i)
-      state = MixColumns(state,True)
-      state = ShiftRows(state,True)
-      state = SybBytes(state,True)
-    state = AddRoundKey(state, 0)
-  else:
-    state = AddRoundKey(state, 0)
-    for i in range(1,Nr):
-      state = SybBytes(state,False)
-      state = ShiftRows(state,False)
-      state = MixColumns(state,False)
-      state = AddRoundKey(state, i)
-    state = SybBytes(state,False)
-    state = ShiftRows(state,False)
-    state = AddRoundKey(state, Nr)
+    if R:
+        state = AddRoundKey(state, Nr)
+        state = ShiftRows(state,True)
+        state = SybBytes(state,True)
+        for i in range(Nr-1, 0, -1):
+            state = AddRoundKey(state, i)
+            state = MixColumns(state,True)
+            state = ShiftRows(state,True)
+            state = SybBytes(state,True)
+        state = AddRoundKey(state, 0)
+    else:
+        state = AddRoundKey(state, 0)
+        for i in range(1, Nr):
+            state = SybBytes(state,False)
+            state = ShiftRows(state,False)
+            state = MixColumns(state,False)
+            state = AddRoundKey(state, i)
+        state = SybBytes(state,False)
+        state = ShiftRows(state,False)
+        state = AddRoundKey(state, Nr)
   
-  return [state[rc % 4][rc // 4] for rc in range(16)]
+    return [state[rc % 4][rc // 4] for rc in range(16)]
 
+# very very very old my api (2019 year)...
 def Hex(Zn,Tr=True):
-  if Tr: Zn = Zn[0]+Zn[1]+Zn[2]+Zn[3]
-  tab = "0123456789abcdef"
-  Str = ""
-  for i in Zn: Str += tab[i // 16] + tab[i % 16]
-  return Str
+    if Tr: Zn = Zn[0]+Zn[1]+Zn[2]+Zn[3]
+    tab = "0123456789abcdef"
+    Str = ""
+    for i in Zn: Str += tab[i // 16] + tab[i % 16]
+    return Str
 def InvHex(Hex):
-  Hex = Hex.lower()
-  tab = "0123456789abcdef"
-  bytes = []
-  for i in range(len(Hex) // 2):
-    Block = Hex[i*2:(i+1)*2]
-    bytes.append(tab.index(Block[0])*16+tab.index(Block[1]))
-  return bytes
+    Hex = Hex.lower()
+    tab = "0123456789abcdef"
+    bytes = []
+    for i in range(len(Hex) // 2):
+        Block = Hex[i*2:(i+1)*2]
+        bytes.append(tab.index(Block[0])*16+tab.index(Block[1]))
+    return bytes
 
 def DecoderAES(data):
-  Str = b""
-  LD = len(data) // 32
-  for i in range(LD):
-    Zn = data[i*32:(i+1)*32]
-    Zn = CoderAES(InvHex(Zn), True)
-    Str += bytes(Zn)
-  ESim = Str[-1]
-  if ESim <= 16:
-    Raz = True
-    for i in range(1,ESim+1):
-      if Str[-i] != ESim: Raz = False
-    if Raz: Str = Str[:-ESim]
-  try: return Str.decode("utf-8")
-  except: return Hex(Str,False).upper()
+    Str = b""
+    LD = len(data) // 32
+    for i in range(LD):
+        Zn = data[i*32 : (i+1)*32]
+        Zn = CoderAES(InvHex(Zn), True)
+        Str += bytes(Zn)
+    ESim = Str[-1]
+    if ESim <= 16:
+        Raz = True
+        for i in range(1, ESim+1):
+            if Str[-i] != ESim: Raz = False
+        if Raz: Str = Str[:-ESim]
+    try: return Str.decode("utf-8")
+    except: return Hex(Str,False).upper()
 
 def EncoderAES(data):
-  if type(data) == str: data = data.encode("utf-8")
-  if type(data) == bytes: data = list(data)
+    if type(data) == str: data = data.encode("utf-8")
+    if type(data) == bytes: data = list(data)
   
-  Dop = (16-len(data) % 16)
-  data += [Dop] * Dop
-  Str = ""
-  for i in range(len(data)//16):
-    Zn = data[i*16:(i+1)*16]
-    Zn = CoderAES(Zn, False)
-    Str += Hex(Zn, False)
-  return Str
+    Dop = (16-len(data) % 16)
+    data += [Dop] * Dop
+    Str = ""
+    for i in range(len(data)//16):
+        Zn = data[i*16:(i+1)*16]
+        Zn = CoderAES(Zn, False)
+        Str += Hex(Zn, False)
+    return Str
 
 def SetKey(key, hex):
-  global KeyC
-  if hex: key = InvHex(key)
-  KeyC = KeyExpansion(key)
+    global KeyC
+    if hex: key = InvHex(key)
+    KeyC = KeyExpansion(key)
 
-#SetKey("a0428e4bc328e248c98a839cf82f983a", True)
-#Str = EncoderAES("LOLOS")
-#Str2 = DecoderAES(Str)
-#print(Str, Str2)
+
+if __name__ == "__main__":
+    SetKey("a0428e4bc328e248c98a839cf82f983a", True)
+    Str = EncoderAES("LOLOS")
+    Str2 = DecoderAES(Str)
+    print(Str, Str2)
--- secrets/ASDsecrets.py
+++ secrets/ASDsecrets.py
@@ -7,18 +7,18 @@ import os
 import sys
 import pickle
 from random import randint
+from typing import Any
 
-import ASDcoderAES # написан мною ещё в 2019 году, оптимизирован в 2021 году
-import mygram      # тот же AES, но с ошеломительной оптимизацией от разработчиков telegram! Бонусом ige256, cbc256, TDF reader и KeyFile reader
+import ASDcoderAES  # written by me back in 2019, optimized in 2021
+import mygram       # same AES, but with outstanding optimization from Telegram developers! Bonus: ige256, cbc256, TDF-reader and KeyFile-reader
 
-ASDcoderAES.Nr = 14 # а было 10, т.к. в Java, да и в целом, настроено именно так...
-
+ASDcoderAES.Nr = 14  # originally 10; changed to 14 because in Java and elsewhere it's configured this way...
 
 
 def test():
     password = "MeowMeowMeow!"
     key = sha256(password.encode("utf-8")).digest()
-    assert len(key) == 32 # именно такая длина и требуется в AES
+    assert len(key) == 32  # exactly the length required for AES
 
     # eK -> expandedKey
     eK1 = ASDcoderAES.KeyExpansion(key)
@@ -28,19 +28,19 @@ def test():
 
     viewA = tuple(zip(*eK1))
     viewB = tuple(tuple(struct.pack(">I", num)) for num in eK2)
-    print(len(viewA), len(viewB)) # 60 60
+    print(len(viewA), len(viewB))  # 60 60
     print(viewA)
     print(viewB)
-    # вывод: это всё равно разные KeyExpansion... Хоть и начало очень похоже
-    # они как-то умудрились sbox встроить прямо в ключ!
+    # conclusion: they still produce different KeyExpansion... Though the beginning looks similar
+    # they somehow managed to bake the sbox directly into the key!
 
     T1 = time()
 
     ASDcoderAES.KeyC = eK1
     encoded = ASDcoderAES.EncoderAES("test message")
     decoded = ASDcoderAES.DecoderAES(encoded)
-    print("encoded:", encoded) # bfed357e69d4d1ff0e95909eee3e9a59
-    print("decoded:", decoded) # test message
+    print("encoded:", encoded)  # bfed357e69d4d1ff0e95909eee3e9a59
+    print("decoded:", decoded)  # test message
 
     T2 = time()
 
@@ -50,30 +50,29 @@ def test():
 
     encoded = mygram.aes256_encrypt(text,    eK2)
     decoded = mygram.aes256_decrypt(encoded, ek3)
-    print("tg encoded:", encoded.hex()) # e470bdff5ea010dee96a2af48aaf2f91
-    print("tg decoded:", decoded[:-decoded[-1]].decode("utf-8")) # test message
-    # вывод: хоть в обоих случаях это AES с 14-мя раундами, но они почему-то разные...
+    print("tg encoded:", encoded.hex())  # e470bdff5ea010dee96a2af48aaf2f91
+    print("tg decoded:", decoded[:-decoded[-1]].decode("utf-8"))  # test message
+    # conclusion: although both use AES with 14 rounds, they are somehow different...
 
     T3 = time()
 
     iv = b"\0" * 16
     assert mygram.cbc256(text, key, iv, True) == encoded
 
-    print("my AES:", T2 - T1) # 0.017785310745239258
-    print("tg AES:", T3 - T2) # 0.018413782119750977, в смысле чуть медленнее?!?!?!?!?
-
+    print("my AES:", T2 - T1)  # 0.017785310745239258
+    print("tg AES:", T3 - T2)  # 0.018413782119750977, i.e. slightly slower?!?!?!?!?
 
 
 def get_appdata_path() -> str:
     if sys.platform.startswith("win"):
-        # В Windows используем переменную окружения APPDATA
-        return os.environ.get("APPDATA", os.path.expanduser("~\\AppData\\Roaming"))
+        # On Windows use the APPDATA environment variable
+        return os.environ.get("APPDATA", os.path.expanduser(r"~\AppData\Roaming"))
     else:
-        # В Linux/macOS чаще всего используется ~/.config
+        # On Linux/macOS, ~/.config is the common choice
         return os.environ.get("XDG_CONFIG_HOME", os.path.expanduser("~/.config"))
 
 class Storage:
-    def __init__(self, name, force = True):
+    def __init__(self, name: str, force: bool = True):
         self.force = force
         self.salt = None
 
@@ -84,7 +83,7 @@ class Storage:
         self.pw_base = {}
         self.load_passwords()
 
-    def password_to_ek(self, password):
+    def password_to_ek(self, password: str) -> list[int]:
         if self.force:
             salt = self.salt
             if salt is None:
@@ -95,9 +94,9 @@ class Storage:
         assert len(key) == 32
         return mygram.aes256_set_encryption_key(key)
 
-    def store_password(self, key, eK):
-        # print(self.path, key in self.pw_base)
-        if key in self.pw_base: return
+    def store_password(self, key: str, eK: list[int]):
+        if key in self.pw_base:
+            return
         with open(self.path, "ab") as file:
             pickle.dump((key, eK), file, protocol=4)
         self.pw_base[key] = eK
@@ -110,15 +109,15 @@ class Storage:
                     self.pw_base[key] = eK
         except (EOFError, FileNotFoundError): pass
 
-    def check_password(self, key, name = None):
+    def check_password(self, key: str, name: str|None = None):
         try: return True, self.pw_base[key]
         except KeyError: pass
 
-        password = input(f"Введите пароль для {name or key!r}: ")
+        password = input(f"Enter password for {name or key!r}: ")
         eK = self.password_to_ek(password)
         return False, eK
 
-    def store(self, obj, password, path, name = None):
+    def store(self, obj: Any, password: str|None, path: str, name: str|None = None):
         key = os.path.abspath(path)
         if password is None:
             _, eK = self.check_password(key, name)
@@ -142,13 +141,14 @@ class Storage:
 
         self.store_password(key, eK)
 
-    def load(self, path, name = None):
+    def load(self, path: str, name: str|None = None) -> Any:
         try:
             with open(path, "rb") as file:
                 iv = file.read(16)
                 if self.force: self.salt = file.read(32)
                 encoded = file.read()
-        except FileNotFoundError: return
+        except FileNotFoundError:
+            return
 
         while True:
             key = os.path.abspath(path)
@@ -158,19 +158,20 @@ class Storage:
             decoded = mygram.cbc256(encoded, eK2, iv, False, is_eK = True)
 
             try:
-                # result = pickle.loads(decoded[:-decoded[-1]])
-                result = pickle.loads(decoded) # удаление pad можно пропустить, из-за особенности pickle
+              # result = pickle.loads(decoded[:-decoded[-1]])
+                result = pickle.loads(decoded)  # removing the pad can be skipped due to pickle's behaviour
                 break
             except Exception:
-                msg = f"Неверный пароль от {name or key!r}!"
-                if stored: raise ValueError(msg)
+                msg = f"Wrong password for {name or key!r}!"
+                if stored:
+                    raise ValueError(msg)
                 print(msg)
 
         self.store_password(key, eK)
         return result
 
-    def to_force(self, path, name = None):
-        assert not self.force, "Storage уже в режиме усиленного пароля!"
+    def to_force(self, path: str, name: str|None = None):
+        assert not self.force, "Storage is already in forced password mode!"
 
         data = self.load(path, name)
         assert data is not None
@@ -181,8 +182,9 @@ class Storage:
         self.force = True
 
         self.store(data, None, path, name)
-        print("Теперь можно сменить force на True в конструкторе Storage!")
+        print("Now you can switch force to True in the Storage constructor!")
         exit()
+
 
 def test2():
     storage = Storage("test.asd")
